d1528168beb5305c2af93e4761c973263643ad21
dbruno <dbruno@autodoc.com.br>
2019-09-27 17:26:02 -0300

    curso js

---
+++ Modulo E/Aula13/index.js
@@ -0,0 +1,18 @@
+var c = 1;
+
+while(c <= 6){
+    console.log('tudo bem?');
+    c++;
+}
+
+// Outro Jeito de contar usando do while
+
+// var c =1;
+// do {
+//     console.log('tudo bem?');
+//     c++; 
+// } while(c <= 6)
+
+
+
+
